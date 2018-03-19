32137fcab444a5d9afe039403cc5293748bb31e6
David Meister <thedavidmeister@gmail.com>
2018-03-19 19:53:52 +1100

    dynamic since to fix broken reporting

--- src/toggl2jira/core.clj
+++ src/toggl2jira/core.clj
@@ -70,12 +70,15 @@
 
 ; Jira data processing.
 
+(defn since
+ []
+ ; https://github.com/toggl/toggl_api_docs/issues/302
+ (str (clj-time.core/minus (clj-time.core/now) (clj-time.core/months 11))))
+
 (defn toggl-items
  []
- (prn client-id)
  (let [api-response (toggl.report/api! "details" {:query-params {:client_ids [client-id]
-                                                                 :since "2017-01-01"
-                                                                 :page 1}})]
+                                                                 :since (since)}})]
   (no-empty-projects api-response)))
 
 (defn jira-times-by-issue
